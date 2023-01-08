af56725722bae2dc5a5111e80cef8be5b2e6687c
unknown <hu649@purdue.edu>
2023-01-07 17:28:47 -0800

    Use F import for torch one hot

--- robust_loss.py
+++ robust_loss.py
@@ -13,7 +13,7 @@ class ReverseCrossEntropy(torch.nn.Module):
     def forward(self, pred, labels):
         pred = F.softmax(pred, dim=1)
         pred = torch.clamp(pred, min=1e-7, max=1.0)
-        label_one_hot = torch.nn.functional.one_hot(labels, self.num_classes).float().to(pred)
+        label_one_hot = F.one_hot(labels, self.num_classes).float().to(pred)
         label_one_hot = torch.clamp(label_one_hot, min=1e-4, max=1.0)
         rce = (-1*torch.sum(pred * torch.log(label_one_hot), dim=1))
         return self.scale * rce.mean()
@@ -28,7 +28,7 @@ class NormalizedCrossEntropy(torch.nn.Module):
 
     def forward(self, pred, labels):
         pred = F.log_softmax(pred, dim=1)
-        label_one_hot = torch.nn.functional.one_hot(labels, self.num_classes).float().to(pred)
+        label_one_hot = F.one_hot(labels, self.num_classes).float().to(pred)
         nce = -1 * torch.sum(label_one_hot * pred, dim=1) / (- pred.sum(dim=1))
         return self.scale * nce.mean()
 
